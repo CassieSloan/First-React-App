171ad7c5ecf71a56dbca816c6f72af15a8dd63c3
Cassie <cassandra.sloan16@gmail.com>
2019-12-19 11:30:45 +1100

    deleted filed in src folder and inserted index.js. manually rendered jsx  to DOM

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//react doesn't use "require" method. Below is how we import packages
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//ReactDom binds funciton to browser and renders. takes two args - what to render and where to render it
+ReactDom.render(
+    // React.createContext("h1", null, "does this work?")
+        //OR
+    <h1>Hi there</h1>,
+    // h1's aren't html, it's jsx (javascript xml). Syntactical sugar for normal Js functions such as above
+    document.getElementById("root")
+);
